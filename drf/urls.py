eb990e94ce26da576ad1be102afe314b5043234e
DaniznX <danye.castillor15@gmail.com>
2025-10-22 16:18:34 -0300

    changes 22-10

--- drf/urls.py
+++ drf/urls.py
@@ -18,8 +18,28 @@ from django.contrib import admin
 from django.urls import path, include
 from primera_app import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = "Documentacion API primera_app",
+        default_version = 'v1',
+        description = "primera_app",
+        terms_of_service = "https://www.google.com/policies/terms/",
+        contact = openapi.Contact(email="danye.castillor15@gmail.com"),
+        license= openapi.License(name="BSD License"),
+    ),
+    public= True  
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.pagina_html, name='home'),
-    path('primera_app/', include('primera_app.urls'))
+    path('primera_app/', include('primera_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger',
+    cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+    cache_timeout=0), name='schema-redoc'),
 ]
